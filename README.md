62dddb7949d60891286a30023f6593d2b7bbf2ee
Michael Szmadzinski <smujmaiku@gmail.com>
2017-03-29 20:39:25 -0400

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# Sell Me Now
+
+Creates random curls to domains and searches to keep those pesky ISPs on their toes
+
+## Notes
+
+I suggest adding this to the hourly cron of that raspi you forgot is plugged into your TV
---
+++ sellmenow.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ ! -f dictionary.txt ]
+then
+	echo 'Updating dictionary ...'
+	curl -sq 'https://raw.githubusercontent.com/dwyl/english-words/master/words2.txt' > dictionary.txt
+fi
+
+words="$(cat dictionary.txt | awk 'length($0)>3' | shuf -n 3 | xargs)"
+domain="$(echo ${words} | sed 's/ //g')"
+tld="$(echo 'com\nnet\norg\nedu\ngov' | shuf -n 1)"
+echo "Selling: ${domain}.${tld}"
+
+data="$(head -c $(shuf -i 2000-8000 -n 1) /dev/urandom | base64)"
+res=$(curl -sq --data "${data}" "http://${domain}.${tld}")
+
+if [ -z "$res" ]
+then
+	echo "Lets try a search '${words}' instead"
+	curl -sq "http://www.google.com/#q=${words}" > /dev/null 2>&1
+fi
+
+echo "Done, sell that ISP!"
